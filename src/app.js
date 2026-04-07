eccd7f82690c93b900a233eb994e9093c57e1d88
AishaH <aisha_1@live.se>
2026-04-07 10:33:53 +0200

    Simplify CORS middleware configuration to allow all origins

--- src/app.js
+++ src/app.js
@@ -32,12 +32,7 @@ app.use(async (req, res, next) => {
     next(err);
   }
 });
-app.use(
-  cors({
-    origin: '*',
-    credentials: true 
-  })
-);
+app.use(cors("*"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
