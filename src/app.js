685a9d542b2f23e40dfd5a15124dd32bc9bd77c1
AishaH <aisha_1@live.se>
2026-04-07 10:38:09 +0200

    Remove custom CORS headers middleware for simplified configuration

--- src/app.js
+++ src/app.js
@@ -8,13 +8,6 @@ import mongoose from "mongoose";
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 let isConnected = false;
 
 async function connectDB() {
